ac382085201f511a55432a0608cbf3e27263c274
MohdAdnanAli <71915643+MohdAdnanAli@users.noreply.github.com>
2025-11-15 08:07:00 -0800

    final code play

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello, DevOps World!");
-  res.end("CI/CD pipeline running successfully!");
+  res.end("Hello, DevOps World! CI/CD pipeline running successfully!");
 });
 
 server.listen(port, () => {
