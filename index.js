3e89c3458715bde8b92040e841306b0d620c535c
MohdAdnanAli <adnanali.aa863@gmail.com>
2025-11-14 23:22:09 -0800

    finlized

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.end("CI/CD pipeline running successfully!");
+  res.end("Hello, DevOps World!");
 });
 
 server.listen(port, () => {
